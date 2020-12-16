f3d2cc9e791f35a5a7c2f24a7cb625f4466eae4d
Dmytro Orlov <orlov.dm@gmail.com>
2020-12-16 17:17:21 +0100

    config

--- src/main/scala/de/riskident/upload/UploadMain.scala
+++ src/main/scala/de/riskident/upload/UploadMain.scala
@@ -1,25 +1,35 @@
 package de.riskident.upload
 
+import com.typesafe.config.ConfigFactory
+import distage.config.ConfigModuleDef
 import distage.{Tag, _}
+import izumi.distage.config.AppConfigModule
 import zio._
 import zio.console._
+
+import java.net.URI
 
 object UploadMain extends App {
   def run(args: List[String]) = {
     val program = for {
-      _ <- putStrLn("123")
+      cfg <- ZIO.service[AppCfg]
+      _ <- putStrLn(cfg.downloadLines.toString)
     } yield ()
 
     def provideHas[R: HasConstructor, A: Tag](fn: R => A): Functoid[A] =
       HasConstructor[R].map(fn)
 
-    val definition = new ModuleDef {
+    val definition = new ConfigModuleDef {
+      makeConfig[AppCfg]("app")
       make[UIO[Unit]].from(provideHas(program.provide))
     }
 
+    val defs = Seq(definition, AppConfigModule(ConfigFactory.defaultApplication()))
     Injector[Task]()
-      .produceGet[UIO[Unit]](definition)
+      .produceGet[UIO[Unit]](defs.merge)
       .useEffect
       .exitCode
   }
 }
+
+case class AppCfg(downloadLines: URI)
