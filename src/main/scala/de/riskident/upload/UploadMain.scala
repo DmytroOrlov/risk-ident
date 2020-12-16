60c02e6491251c585cd9de4d34eeefa06b7d38a0
Dmytro Orlov <orlov.dm@gmail.com>
2020-12-16 17:13:11 +0100

    distage

--- src/main/scala/de/riskident/upload/UploadMain.scala
+++ src/main/scala/de/riskident/upload/UploadMain.scala
@@ -1,16 +1,25 @@
 package de.riskident.upload
 
+import distage.{Tag, _}
 import zio._
 import zio.console._
 
 object UploadMain extends App {
   def run(args: List[String]) = {
-    val program = (for {
+    val program = for {
       _ <- putStrLn("123")
-      _ <- IO.fail("err")
-    } yield ())
-      .exitCode
+    } yield ()
 
-    program
+    def provideHas[R: HasConstructor, A: Tag](fn: R => A): Functoid[A] =
+      HasConstructor[R].map(fn)
+
+    val definition = new ModuleDef {
+      make[UIO[Unit]].from(provideHas(program.provide))
+    }
+
+    Injector[Task]()
+      .produceGet[UIO[Unit]](definition)
+      .useEffect
+      .exitCode
   }
 }
